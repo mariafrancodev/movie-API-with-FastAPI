88edf72b91970e8788b9f0e4b50703085b50e427
Maria Franco <mafa1008@gmail.com>
2023-10-25 13:50:00 -0500

    data type and parameter validation

--- main.py
+++ main.py
@@ -1,77 +1,92 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
-
-app.title = "Mi aplicacion con FastAPI"
+app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating:float = Field(ge=1, le=10)
+    category:str = Field(min_length=5, max_length=15)
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                "id": 1,
+                "title": "Mi película",
+                "overview": "Descripción de la película",
+                "year": 2022,
+                "rating": 9.8,
+                "category" : "Acción"
+                }
+            ]
+        }
+    }
 
 movies = [
     {
-        'id': 1,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Acción'    
-    } ,
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
     {
-        'id': 2,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Acción'    
-    } 
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
 ]
 
-@app.get('/', tags=["Home"])
+@app.get('/', tags=['home'])
 def message():
-    return HTMLResponse('<h1>Holi</h1>')
+    return HTMLResponse('<h1>Hello world</h1>')
 
-@app.get('/movies', tags=["movies"])
+@app.get('/movies', tags=['movies'])
 def get_movies():
     return movies
 
-@app.get('/movies/{id}', tags=["movies"])
-def get_movie(id: int):
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int = Path(ge=1, le=2000)):
     for item in movies:
-        if item['id'] == id:
+        if item["id"] == id:
             return item
-        
-@app.get('/movies/', tags=["movies"])
-def get_movies_by_category(category: str, year: int):
-    return [item for item in movies if category == category]
+    return []
 
-@app.post('/movies', tags=["movies"])
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)):
+    return [ item for item in movies if item['category'] == category ]
+
+@app.post('/movies', tags=['movies'])
 def create_movie(movie: Movie):
     movies.append(movie)
     return movies
 
-@app.put('/movies/{id}', tags=["movies"])
-def update_movie(id: int,  movie: Movie):
-    for item in movies:
-        if item['id'] == id:
-            item['title'] = movie.title
-            item['overview'] = movie.overview
-            item['year'] = movie.year
-            item['rating'] = movie.rating
-            item['category'] = movie.category
-    return movies
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, movie: Movie):
+	for item in movies:
+		if item["id"] == id:
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
+			return movies
 
-@app.delete('/movies/{id}', tags=["movies"])
+@app.delete('/movies/{id}', tags=['movies'])
 def delete_movie(id: int):
     for item in movies:
-        if item['id'] == id:
+        if item["id"] == id:
             movies.remove(item)
             return movies
